e00c334b76793681a76fdd773168768334e5da94
ajaragon <julianaragongithub@outlook.es>
2024-05-26 16:13:17 +0200

    Agregada una barra de navegación

--- Ejercicio03/wwwroot/js/site.js
+++ Ejercicio03/wwwroot/js/site.js
@@ -6,3 +6,16 @@
     });
 });
 
+/*Menú de la barra de navegación*/
+const navbarElement = document.querySelector(".nav");
+const hamburguerElement = document.querySelector(".hamburguer");
+
+hamburguerElement.addEventListener("click", () => {
+    navbarElement.classList.toggle("nav--open");
+    hamburguerElement.classList.toggle("hamburguer--open");
+});
+
+navbarElement.addEventListener("click", () => {
+    navbarElement.classList.remove("nav--open");
+    hamburguerElement.classList.remove("hamburguer--open");
+});
