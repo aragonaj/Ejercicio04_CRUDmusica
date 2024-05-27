90ad21e3048b443491be930db75825ffdf1502f3
Andrés Aragón <aragonandresjulian@gmail.com>
2024-05-27 11:51:41 +0200

    Actualización 4.3V estilos

--- Ejercicio03/Models/Funcione.cs
+++ Ejercicio03/Models/Funcione.cs
@@ -6,11 +6,10 @@ namespace Ejercicio03.Models;
 
 public partial class Funcione
 {
-    [Required(ErrorMessage = "Este campo es obligatorio")]
     public int Id { get; set; }
 
     [Required(ErrorMessage ="Este campo es obligatorio")]
-    [StringLength(50, ErrorMessage = "Se excedió el número de caracteres permitido")]
+    [StringLength(30, ErrorMessage = "Se excedió el número de caracteres permitido")]
     public string? Nombre { get; set; }
 
     public virtual ICollection<FuncionesArtista> FuncionesArtista { get; set; } = new List<FuncionesArtista>();
--- Ejercicio03/Models/FuncionesArtista.cs
+++ Ejercicio03/Models/FuncionesArtista.cs
@@ -6,15 +6,13 @@ namespace Ejercicio03.Models;
 
 public partial class FuncionesArtista
 {
-    [Required(ErrorMessage = "Este campo es obligatorio")]
     public int Id { get; set; }
 
-    [Required(ErrorMessage = "Este campo es obligatorio")]
     public int? FuncionesId { get; set; }
 
-    [Required(ErrorMessage = "Este campo es obligatorio")]
-    [StringLength(50, ErrorMessage = "Se excedió el número de caracteres permitido")]
-    [Display(Name = "Artista")]
+    //[Required(ErrorMessage = "Este campo es obligatorio")]
+    //[StringLength(50, ErrorMessage = "Se excedió el número de caracteres permitido")]
+    //[Display(Name = "Artista")]
     public int? ArtistasId { get; set; }
 
     public virtual Artista? Artistas { get; set; }
