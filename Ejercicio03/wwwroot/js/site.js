95763197f3590566d228de7f431466d54180dc7f
Andrés Aragón <aragonandresjulian@gmail.com>
2024-05-27 14:15:28 +0200

    Actualización 4.5V estilos

--- Ejercicio03/wwwroot/js/site.js
+++ Ejercicio03/wwwroot/js/site.js
@@ -1,21 +1,25 @@
 ﻿$(document).ready(function () {
     $('#datatableCheating').DataTable({
         "scrollY": "450",
-        "scrollCollapse": true,
+        "scrollCollapse": false,
         "paging": true, 
     });
 });
 
 /*Menú de la barra de navegación*/
 const navbarElement = document.querySelector(".nav");
+const cortinilla = document.querySelector(".cortinilla");
 const hamburguerElement = document.querySelector(".hamburguer");
 
 hamburguerElement.addEventListener("click", () => {
     navbarElement.classList.toggle("nav--open");
+    cortinilla.classList.toggle("cortinilla--open");
     hamburguerElement.classList.toggle("hamburguer--open");
 });
 
 navbarElement.addEventListener("click", () => {
     navbarElement.classList.remove("nav--open");
+    cortinilla.classList.remove("cortinilla--open");
     hamburguerElement.classList.remove("hamburguer--open");
 });
+
